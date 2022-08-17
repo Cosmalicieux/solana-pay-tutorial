17a982484e0a749fe0c6c1474a808e238c408f34
cosmalicieux <cosma.chvt@protonmail.ch>
2022-08-17 23:02:30 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <footer className="flex w-full justify-center border-t-2 border-white-900 py-12 h-10">
       <a className="text-white" href='https://www.pointer.gg?utm_source=solana-pay' target='_blank' rel='noopener noreferrer'>
-        Learn web3 dev and earn crypto rewards at{" "}
-        <span>Pointer</span>
+        dev build{" "}
+        <span>cosmalicieux</span>
       </a>
     </footer>
   )
